c3d447db914b4aacfb541bca9c937577ceaf523d
Cristian <cristiancristaldo118@gmail.com>
2022-12-02 19:22:27 -0300

    final_v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,68 @@
 import { Component } from '@angular/core';
 
+import { Product } from './product.model';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  widthImg = 10;
   name = 'Cristian';
   age= 25;
   img= 'https://i.pinimg.com/236x/ce/59/83/ce59837dd46efcaa5549a75bf2b1e443.jpg';
   btnDisable= true;
+  register = {
+    name: '',
+    email: '',
+    password: ''
+  }
   persona= {
     name: 'Cristian',
     age: 18,
     avatar: 'https://i.pinimg.com/236x/ce/59/83/ce59837dd46efcaa5549a75bf2b1e443.jpg'
   }
-  names: string[] = ['Nico - 🖤', 'Cristian - 💯', 'Seu - 😜'];
+  names: string[] = ['Juan', 'Carlos', 'Silvana'];
   newName = '';
+  box = {
+    width: 100,
+    height: 100,
+    background: 'red'
+  };
+  products: Product[] = [
+    {
+      name: 'EL mejor juguete',
+      price: 565,
+      image: './assets/images/toy.jpg',
+      category: 'all',
+    },
+    {
+      name: 'Bicicleta casi nueva',
+      price: 356,
+      image: './assets/images/bike.jpg'
+    },
+    {
+      name: 'Colleción de albumnes',
+      price: 34,
+      image: './assets/images/album.jpg'
+    },
+    {
+      name: 'Mis libros',
+      price: 23,
+      image: './assets/images/books.jpg'
+    },
+    {
+      name: 'Casa para perro',
+      price: 34,
+      image: './assets/images/house.jpg'
+    },
+    {
+      name: 'Gafas',
+      price: 3434,
+      image: './assets/images/glasses.jpg'
+    }
+  ]
 
   toggleButton() {
     this.btnDisable= !this.btnDisable;
@@ -43,5 +89,9 @@ export class AppComponent {
 
   deleteName(index: number) {
     this.names.splice(index, 1);
+  }
+
+  onRegister() {
+    console.log(this.register);
   }
 }
